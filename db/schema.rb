736e90e83d75dd15e73bb59b65834e5941266f6e
wrtaff <wrtaff@gmail.com>
2011-02-11 14:06:23 -0800

    finished chapter 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110209201617) do
+ActiveRecord::Schema.define(:version => 20110211212127) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
